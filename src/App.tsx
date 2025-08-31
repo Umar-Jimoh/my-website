0067aeebfd34e1d4739a0b683a22f24e341d9148
Umar Jimoh <umarjimoh@hotmail.com>
2025-08-31 12:53:06 +0100

    Add initial pages (Home, Resume, Blogs, BlogDetails, Contact, SignIn, SignUp)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  Home,
+  Resume,
+  Blogs,
+  BlogDetails,
+  Contact,
+  SignIn,
+  SignUp,
+} from "./Pages";
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/blogs" element={<Blogs />}>
+          <Route path=":id" element={<BlogDetails />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
