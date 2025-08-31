75df71f7a58298e8411510dde363c8dacfff2f11
Umar Jimoh <umarjimoh@hotmail.com>
2025-08-31 12:53:06 +0100

    Add initial pages (Home, Resume, Blogs, BlogDetails, Contact, SignIn, SignUp)

---
+++ src/Pages/index.ts
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Resume from "./Resume";
+import Blogs from "./Blogs";
+import BlogDetails from "./BlogDetails";
+import Contact from "./Contact";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+
+export { Home, Resume, Blogs, BlogDetails, Contact, SignIn, SignUp };
