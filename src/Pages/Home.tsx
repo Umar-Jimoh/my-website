2f8fecaae5350917fd4213f9ab7c7aae1fd14bd1
Umar Jimoh <umarjimoh@hotmail.com>
2025-09-01 23:24:06 +0100

    feat: add homepage content with centered hero section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,17 @@ import MainLayout from "@/layouts/MainLayout";
 function Home() {
   return (
     <MainLayout isLogo={false}>
-      <div>Home</div>
+      <div className="flex flex-col justify-center items-center min-h-[calc(100dvh-64px)] text-center">
+        <span className=" md:text-md mb-10 text-muted-foreground">
+          It’s all about an artisan dev
+        </span>
+        <h1 className="text-4xl md:text-5xl uppercase font-bold mt-4">
+          I'm Umar Jimoh
+        </h1>
+        <p className="text-md md:text-lg mt-5">
+          Software Developer based in Nigeria, Abuja
+        </p>
+      </div>
     </MainLayout>
   );
 }
