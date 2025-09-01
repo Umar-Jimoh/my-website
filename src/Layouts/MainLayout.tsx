9cd3f48eee5a7336e6b2231aaff01ae42ee82fc9
Umar Jimoh <umarjimoh@hotmail.com>
2025-09-01 18:00:56 +0100

    feat: add Navigation component with links and styling

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,5 +1,17 @@
-function MainLayout({ children }) {
-  return <main>{children}</main>;
+import Navigation from "@/components/shared/Navigation";
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+  isLogo?: boolean
+}
+
+function MainLayout({ children, isLogo = true }: MainLayoutProps) {
+  return (
+    <div className="min-h-screen">
+      <Navigation isLogo={isLogo}/>
+      <main>{children}</main>
+    </div>
+  );
 }
 
 export default MainLayout;
---
+++ src/components/shared/Navigation.tsx
@@ -0,0 +1,46 @@
+import { navItems } from "@/lib/constants";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+interface NavigationProps {
+  isLogo: boolean;
+}
+
+function Navigation({ isLogo }: NavigationProps) {
+  return (
+    <nav className="sticky top-0 bg-background/50 backdrop-blur-md">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
+          {/* Logo */}
+          <div>
+            {isLogo && (
+              <Link to={"/"}>
+                <h1 className="text-xl uppercase font-bold text-foreground font-serif">
+                  Umar Jimoh
+                </h1>
+              </Link>
+            )}
+          </div>
+          {/* Desktop Navigation */}
+          <div className="hidden md:flex items-center space-x-6">
+            {navItems.map((item) => (
+              <NavLink
+                key={item.name}
+                to={item.href}
+                className={({
+                  isActive,
+                }) => `p-2 text-sm font-medium transition-colors duration-200
+                ${
+                  !isActive && "text-muted-foreground hover:text-foreground"
+                }`}>
+                {item.name}
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
